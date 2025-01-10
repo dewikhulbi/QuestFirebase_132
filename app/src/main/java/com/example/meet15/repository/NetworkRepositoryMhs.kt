5d893bd2bbdee705e3ff34dce9ab8bb7fbb0ccac
dewikhulbi <dewikhlb20@gmail.com>
2025-01-11 01:02:55 +0700

    Memperbarui data mahasiswa di Firestore berdasarkan nim.

--- app/src/main/java/com/example/meet15/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/meet15/repository/NetworkRepositoryMhs.kt
@@ -60,6 +60,14 @@ class NetworkRepositoryMhs (
     }
 
 
-
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        try { firestore.collection("Mahasiswa")
+            .document(mahasiswa.nim)
+            .set(mahasiswa)
+            .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal mengupdate data mahasiswa:${e.message}")
+        }
+    }
 
 }
