e0a8429a8e8b3a14685bc107e088194bb3fb74eb
dewikhulbi <dewikhlb20@gmail.com>
2025-01-11 00:55:38 +0700

    Menambahkan data mahasiswa baru ke Firestore.

---
+++ app/src/main/java/com/example/meet15/repository/NetworkRepositoryMhs.kt
@@ -0,0 +1,22 @@
+package com.example.meet15.repository
+
+import com.example.meet15.model.Mahasiswa
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
+
+class NetworkRepositoryMhs (
+    private val firestore: FirebaseFirestore
+) : RepositoryMhs {
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+        try { firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data mahasiswa:${e.message}")
+        }
+    }
+
+
+}
