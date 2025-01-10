8c9bf21f81764b12bc8b7bd0d7142df8938ccf9c
dewikhulbi <dewikhlb20@gmail.com>
2025-01-11 01:02:29 +0700

    Menghapus data mahasiswa dari Firestore berdasarkan nim

--- app/src/main/java/com/example/meet15/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/meet15/repository/NetworkRepositoryMhs.kt
@@ -49,6 +49,16 @@ class NetworkRepositoryMhs (
         }
     }
 
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        try { firestore.collection("Mahasiswa")
+            .document(mahasiswa.nim)
+            .delete()
+            .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa: ${e.message}")
+        }
+    }
+
 
 
 
