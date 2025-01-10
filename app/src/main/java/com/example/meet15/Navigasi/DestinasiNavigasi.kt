5f7e5910d225c11dd6f02385e450f5ea100c9fa5
dewikhulbi <dewikhlb20@gmail.com>
2025-01-11 01:05:39 +0700

    navigasi untuk layar Insert

--- app/src/main/java/com/example/meet15/Navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/meet15/Navigasi/DestinasiNavigasi.kt
@@ -9,4 +9,7 @@ object DestinasiHome : DestinasiNavigasi {
     override val route: String = "home"
     override val titleRes: String = "Home"
 }
-
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
