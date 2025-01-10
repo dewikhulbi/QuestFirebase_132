1e3d750f10f22a324bed10a05e8d95c71d695639
dewikhulbi <dewikhlb20@gmail.com>
2025-01-11 00:49:32 +0700

    membuat instance containerapp

--- app/src/main/java/com/example/meet15/MahasiswaApp.kt
+++ app/src/main/java/com/example/meet15/MahasiswaApp.kt
@@ -7,5 +7,10 @@ class MahasiswaApp : Application() {
     //fungsinya untuk menympan instance ContainerApp
     lateinit var containerApp: MahasiswaContainer
 
-    
+    override fun onCreate() {
+        super.onCreate()
+        //membuat instance containerapp
+        containerApp = MahasiswaContainer(this)
+        //instance adalah object yang dibuat dari class
+    }
 }
