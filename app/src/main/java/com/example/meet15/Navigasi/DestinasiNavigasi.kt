242d3514a7bf4aaaedc9366565bc8146f62973f6
dewikhulbi <dewikhlb20@gmail.com>
2025-01-11 01:05:13 +0700

    navigasi untuk layar Home

---
+++ app/src/main/java/com/example/meet15/Navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,12 @@
+package com.example.meet15.Navigasi
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+
